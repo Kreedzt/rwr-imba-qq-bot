9346c4b78607c11036e626705911dc67b9122018
Kreedzt <zhaozisong1@live.com>
2023-03-20 14:05:26 +0800

    :sparkles: Kreedzt - add website cd

--- src/commands/website/register.ts
+++ src/commands/website/register.ts
@@ -3,7 +3,8 @@ import { getReplyOutput } from "./utils";
 
 export const WebsiteCommandRegister: IRegister = {
     name: "website",
-    description: "帮助网站列表",
+    description: "帮助网站列表.[10s CD]",
+    timesInterval: 10,
     isAdmin: false,
     exec: async (ctx) => {
         await ctx.reply(getReplyOutput(ctx.env));
