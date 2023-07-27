3c611344f26cd9064c26f5d90545079e8d29d8f5
Kreedzt <zhaozisong1@live.com>
2023-07-27 17:22:21 +0800

    :sparkles: fixed coverage cli

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,66 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { VitePluginNode } from 'vite-plugin-node';
 
 export default defineConfig({
-    // ...vite configure
     plugins: [
         ...VitePluginNode({
-            // Nodejs native Request adapter
-            // currently this plugin support 'express', 'nest', 'koa' and 'fastify' out of box,
-            // you can also pass a function if you are using other frameworks, see Custom Adapter section
             adapter: 'express',
-
-            // tell the plugin where is your project entry
             appPath: './src/index.ts',
-e
-            // Optional, default: 'viteNodeApp'
-            // the name of named export of you app from the appPath file
             exportName: 'app',
-
-            // Optional, default: 'esbuild'
-            // The TypeScript compiler you want to use
-            // by default this plugin is using vite default ts compiler which is esbuild
-            // 'swc' compiler is supported to use as well for frameworks
-            // like Nestjs (esbuild dont support 'emitDecoratorMetadata' yet)
-            // you need to INSTALL `@swc/core` as dev dependency if you want to use swc
-            tsCompiler: 'esbuild',
-
-            // Optional, default: {
-            // jsc: {
-            //   target: 'es2019',
-            //   parser: {
-            //     syntax: 'typescript',
-            //     decorators: true
-            //   },
-            //  transform: {
-            //     legacyDecorator: true,
-            //     decoratorMetadata: true
-            //   }
-            // }
-            // }
-            // swc configs, see [swc doc](https://swc.rs/docs/configuration/swcrc)
-            swcOptions: {},
         }),
     ],
-    optimizeDeps: {
-        // Vite does not work well with optionnal dependencies,
-        // you can mark them as ignored for now
-        // eg: for nestjs, exlude these optional dependencies:
-        // exclude: [
-        //   '@nestjs/microservices',
-        //   '@nestjs/websockets',
-        //   'cache-manager',
-        //   'class-transformer',
-        //   'class-validator',
-        //   'fastify-swagger',
-        // ],
-    },
     test: {
         globals: true,
+        watch: false,
         coverage: {
             all: true,
             include: ['src'],
-            provider: 'c8', // or 'c8',
+            provider: 'istanbul', // or 'c8',
             reporter: ['text', 'json', 'cobertura', 'html'],
         },
         includeSource: ['src'],
