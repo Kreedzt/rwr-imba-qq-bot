36d1c63f4e7018f42407028c12a84c5304361388
Kreedzt <zhaozisong1@live.com>
2023-07-30 20:53:50 +0800

    :sparkles: tdoll: ignore case & -

--- src/commands/tdoll/utils.test.ts
+++ src/commands/tdoll/utils.test.ts
@@ -14,7 +14,13 @@ const MOCK_DATA: ITDollDataItem[] = [
         "枪名": "M16A1",
         "link": "http://www.gfwiki.org/w/M16A1",
         "枪种": "突击步枪",
-    }
+    },
+    {
+        "编号": "No.53",
+        "枪名": "NTW-20",
+        "link": "http://www.gfwiki.org/w/NTW-20#MOD3",
+        "枪种": "步枪"
+      }
 ];
 
 describe('tdoll: formatTdollData', () => {
@@ -40,6 +46,23 @@ describe('tdoll: getTdollDataRes', () => {
         const res = getTdollDataRes(MOCK_DATA, query);
 
         expect(res).toBe(`No.55 M4A1 突击步枪\nhttp://www.gfwiki.org/w/M4A1#MOD3\n\n最多展示 5 项结果`);
+    });
+
+    
+    it.concurrent('query found 1 result, ignore case', () => {
+        const query = 'm4a1';
+
+        const res = getTdollDataRes(MOCK_DATA, query);
+
+        expect(res).toBe(`No.55 M4A1 突击步枪\nhttp://www.gfwiki.org/w/M4A1#MOD3\n\n最多展示 5 项结果`);
+    });
+
+    it.concurrent('query found 1 result, ignore "-"', () => {
+        const query = 'ntw20';
+
+        const res = getTdollDataRes(MOCK_DATA, query);
+
+        expect(res).toBe(`No.53 NTW-20 步枪\nhttp://www.gfwiki.org/w/NTW-20#MOD3\n\n最多展示 5 项结果`);
     });
 
     it.concurrent('query found 2 results', () => {
