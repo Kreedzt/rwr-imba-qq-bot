9f226fb28bff48831db914a7bdf6c293e8a01ddf
Kreedzt <zhaozisong1@live.com>
2024-05-10 23:01:36 +0800

    :memo: update empty tdollskin response

--- src/commands/tdoll/register.ts
+++ src/commands/tdoll/register.ts
@@ -16,7 +16,7 @@ export const TDollCommandRegister: IRegister = {
     exec: async (ctx) => {
         if (ctx.params.size < 1 || ctx.params.size > 2) {
             await ctx.reply(
-                '参数不正确, 示例: #tdoll M4A1, #tdoll random(random 为随机返回), #tdoll m4 ar(查询突击步枪)'
+                '参数不正确, 示例: #tdoll M4A1, #tdoll random(random 为随机返回), #tdoll m4 ar(查询突击步枪), #tdoll random ar(随机突击步枪)'
             );
             return;
         }
