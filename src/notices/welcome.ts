89600c9e2322df59533aefa1499001558f687194
Kreedzt <zhaozisong1@live.com>
2026-02-02 10:00:25 +0800

    :wrench: update welcome file content

--- src/notices/welcome.ts
+++ src/notices/welcome.ts
@@ -1,15 +1,12 @@
 import * as fs from 'fs';
-import * as path from 'path';
 import { NoticeExecCtx } from "../types";
-import { getDirname } from '../utils/esm';
 
-const __dirname = getDirname(import.meta.url);
 let template = '';
 
 export const welcomeNewMember = async (ctx: NoticeExecCtx) => {
     if (!template) {
         const templateFileName = ctx.env.WELCOME_TEMPLATE;
-        const content = fs.readFileSync(path.join(__dirname, templateFileName), 'utf-8');
+        const content = fs.readFileSync(templateFileName, 'utf-8');
         template = content;
     }
     await ctx.reply(template);
